e888b01517a5ecb6e9df926e5e4e5930b7b119a9
김태우 <taewoo080401@gmail.com>
2025-07-15 17:26:10 +0900

    feat: delete firebase

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,47 +1,7 @@
-"use client";
-
-import {
-  GithubAuthProvider,
-  GoogleAuthProvider,
-  signInWithPopup,
-} from "firebase/auth";
-import { auth } from "@/libs/firebase/firebase-config";
-import { FirebaseError } from "firebase/app";
-import { toast } from "@/components/provider/ToastProvider";
-import { customFetch } from "@/libs/fetch/customFetch";
-
 const Login = () => {
-  const handleLogin = async (provider: "google" | "github") => {
-    try {
-      const result = await signInWithPopup(
-        auth,
-        provider === "google"
-          ? new GoogleAuthProvider()
-          : new GithubAuthProvider()
-      );
-      const user = result.user;
-      const idToken = await user.getIdTokenResult();
-      if(idToken) {
-        console.log(idToken)
-        const data = await customFetch.post("/auth/verify", { idToken: idToken.token });
-        console.log(data);
-      }
-    } catch (error) {
-      const err = error as FirebaseError;
-      if (err.code === "auth/account-exists-with-different-credential") {
-        toast.error(`이미 가입된 계정입니다.`);
-        return;
-      }
-      toast.error("네트워크 에러");
-    }
-  };
-
   return (
-    <div>
-      <button onClick={() => handleLogin("google")}>구글 로그인</button>
-      <button onClick={() => handleLogin("github")}>깃허브 로그인</button>
-    </div>
-  );
-};
+    <div>Login</div>
+  )
+}
 
-export default Login;
+export default Login
