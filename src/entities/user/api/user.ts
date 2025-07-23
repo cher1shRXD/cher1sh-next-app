a90ecdb446ed9d90c082a1422569270e485182f7
김태우 <113758615+cher1shRXD@users.noreply.github.com>
2025-07-23 14:57:56 +0900

    fix: error handler

--- src/entities/user/api/user.ts
+++ src/entities/user/api/user.ts
@@ -2,6 +2,10 @@ import { customFetch } from "@/shared/libs/custom-fetch";
 import { User } from "../types/user";
 
 export const fetchUser = async () => {
-  const { data } = await customFetch.get<User>('/users/me');
-  return data;
-}
+  try {
+    const { data } = await customFetch.get<User>("/users/me");
+    return data;
+  } catch {
+    return null;
+  }
+};
